fb308323a25055068654c41366fd65b8bad41637
MdAfsarHossain <afsar291722@gmail.com>
2026-04-13 11:03:09 +0600

    feat: command feature added

---
+++ command.js
@@ -0,0 +1,5 @@
+// docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
+// npm init -y
+// npm i express nodemon redis axios
+// https://redis.io/
+// https://redis.io/docs/latest/operate/oss_and_stack/install/install-stack/docker/
